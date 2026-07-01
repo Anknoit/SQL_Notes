4b7392e6aa0cf185a265536baea514b07dcc47d5
Anknoit <vayugoankit@gmail.com>
2026-07-01 20:30:20 +0530

    JOINS with UNIONS

--- tutorial/JOINS.sql
+++ tutorial/JOINS.sql
@@ -18,4 +18,34 @@ GROUP BY
     
 -- SELECT = GROUP BY columns + Aggregate functions only.
 -- Think of GROUP BY as creating one row per group.
+
+
+-- Q2. Display all sales along with the customer's first and last name.
+SELECT
+	s.sales_id,
+    c.first_name, 
+    c.last_name,
+    s.product_key
+FROM 
+	fact_sales s
+LEFT JOIN
+	dim_customer c
+ON s.sales_id = c.customer_key
+WHERE c.first_name IS NOT NULL;
+
+-- Show product name and quantity sold for every sale.
+SELECT 
+	p.product_name,
+    s.quantity_sold
+FROM 
+	dim_product p
+LEFT JOIN
+	fact_sales s
+ON 
+	p.product_key = s.sales_id
+WHERE s.quantity_sold IS NOT NULL;
+
+
+-- FULL JOIN NOT SUPPORTED, use UNION of LEFT AND RIGHT JOIN to acheive FULL JOIN
+	
 	
